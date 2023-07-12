5d526fb613061e3ecc6fb81de6e7ebc06c94860c
Hoyoung Jung <honux77@gmail.com>
2023-07-12 17:38:49 +0900

    테스트 1 구현 - 클래스의 필드, 생성자, 메소드 이름 출력 구현

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,12 +1,15 @@
 package next.reflection;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ReflectionTest {
@@ -14,10 +17,32 @@ public class ReflectionTest {
 
     @Test
     @DisplayName("테스트1: 리플렉션을 이용해서 클래스와 메소드의 정보를 정확하게 출력해야 한다.")
+    //Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다
     public void showClass() {
         SoftAssertions s = new SoftAssertions();
         Class<Question> clazz = Question.class;
-        logger.debug("Classs Name {}", clazz.getName());
+        logger.debug("Class Name {}", clazz.getName());
+
+        //print all field info using stream
+        logger.debug("필드 정보");
+        Arrays.stream(clazz.getDeclaredFields()).forEach(field -> {
+            logger.debug("\t이름: {} 접근한정자: {}", field.getName(), Modifier.toString(field.getModifiers()));
+        });
+
+        //print all constructor info using stream
+        logger.debug("생성자 정보");
+        Arrays.stream(clazz.getDeclaredConstructors()).forEach(constructor -> {
+            logger.debug("\t이름: {} 접근한정자: {}", constructor.getName(), Modifier.toString(constructor.getModifiers()));
+        });
+
+        //print all method info using stream
+        logger.debug("메소드 정보");
+        Arrays.stream(clazz.getDeclaredMethods()).forEach(method -> {
+            logger.debug("\t이름: {} 접근한정자: {}", method.getName(),
+                    Modifier.toString(method.getModifiers()));
+        });
+
+
     }
 
     @Test
